e6333f2973c87c34050ae24d118e6e5d004da7d4
Use your GitHub damianhunt <Enter your dhuntsa@gmail.com>
2022-04-23 17:20:50 +0200

    commit

--- js/home.js
+++ js/home.js
@@ -27,11 +27,10 @@ const dayName = daynames[d.getDay()];
 const monthName = months[d.getMonth()];
 const year = d.getFullYear();
 //const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+const fulldate = `${year}`;
 // using getElementById
 document.getElementById("currentdate").textContent = fulldate;
-const currentyear = `${year}`;
-document.getElementById("currentyear").textContent = currentyear;
+
 // ****************************
 // using querySelector
 const date2 = document.querySelector('#currentdate2');
@@ -46,7 +45,7 @@ try {
 var
   nLastVisit = parseFloat(document.cookie.replace(/(?:(?:^|.*;)\s*last_modif\s*\=\s*([^;]*).*$)|^.*$/, "$1")),
   nLastModif = Date.parse(document.lastModified);
-
+document.getElementById("lastmod").textContent = nLastModif;
 if (isNaN(nLastVisit) || nLastModif > nLastVisit) {
   document.cookie = "last_modif=" + Date.now() + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=" + location.pathname;
 
