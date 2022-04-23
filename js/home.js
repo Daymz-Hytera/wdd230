242a1c9bcd9e6616d9bb77d5b3f3719161797a55
Use your GitHub damianhunt <Enter your dhuntsa@gmail.com>
2022-04-23 19:42:33 +0200

    commit

--- js/home.js
+++ js/home.js
@@ -31,25 +31,8 @@ const fulldate = `${year}`;
 // using getElementById
 document.getElementById("currentdate").textContent = fulldate;
 
-// ****************************
-// using querySelector
-const date2 = document.querySelector('#currentdate2');
 
-try {
-  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-  date2.textContent = new Date().toLocaleDateString('en-UK', options);
-} catch (e) {
-  alert('Error with code or your browser does not support Locale');
-}
+//const formatdate =`${d.getMonth()} / ${d.getDate()} / ${year} ${d.getTime()}`;
 
-var
-  nLastVisit = parseFloat(document.cookie.replace(/(?:(?:^|.*;)\s*last_modif\s*\=\s*([^;]*).*$)|^.*$/, "$1")),
-  nLastModif = Date.parse(document.lastModified);
-document.getElementById("lastmod").textContent = nLastModif;
-if (isNaN(nLastVisit) || nLastModif > nLastVisit) {
-  document.cookie = "last_modif=" + Date.now() + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=" + location.pathname;
-
-  if (isFinite(nLastVisit)) {
-    alert("This page has been changed!");
-  }
-}
+const oLastModif = new Date(document.lastModified);
+document.getElementById("lastmod").textContent = oLastModif;
