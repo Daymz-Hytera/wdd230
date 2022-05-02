d4898707322ab56f833c88a7f1aa4fe0fc1c0c7c
damianhunt <dhuntsa@gmail.com>
2022-05-02 13:52:27 +0200

    commit

--- js/date.js
+++ js/date.js
@@ -45,6 +45,8 @@ var
   nLastVisit = parseFloat(document.cookie.replace(/(?:(?:^|.*;)\s*last_modif\s*\=\s*([^;]*).*$)|^.*$/, "$1")),
   nLastModif = Date.parse(document.lastModified);
 
+  document.getElementById("lastmod").textContent = nLastModif
+
 if (isNaN(nLastVisit) || nLastModif > nLastVisit) {
   document.cookie = "last_modif=" + Date.now() + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=" + location.pathname;
 
