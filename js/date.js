c28b134fa7887e4c12699ebc5ad86b879ddeef2c
damianhunt <dhuntsa@gmail.com>
2022-05-02 13:50:47 +0200

    commit

--- js/date.js
+++ js/date.js
@@ -34,5 +34,21 @@ document.getElementById("yeardate").textContent = yeardate;
 
 //const formatdate =`${d.getMonth()} / ${d.getDate()} / ${year} ${d.getTime()}`;
 
-const oLastModif = new Date(document.lastModified);
-document.getElementById("lastmod").textContent = oLastModif;
+//const oLastModif = new Date(document.lastModified);
+//document.getElementById("lastmod").textContent = oLastModif;
+
+alert(document.lastModified);
+let oLastModif = new Date(document.lastModified);
+//let nLastModif = Date.parse(document.lastModified);
+
+var
+  nLastVisit = parseFloat(document.cookie.replace(/(?:(?:^|.*;)\s*last_modif\s*\=\s*([^;]*).*$)|^.*$/, "$1")),
+  nLastModif = Date.parse(document.lastModified);
+
+if (isNaN(nLastVisit) || nLastModif > nLastVisit) {
+  document.cookie = "last_modif=" + Date.now() + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=" + location.pathname;
+
+  if (isFinite(nLastVisit)) {
+    alert("This page has been changed!");
+  }
+}
