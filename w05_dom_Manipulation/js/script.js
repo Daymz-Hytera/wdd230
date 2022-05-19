1e96b99c318366b1ddfc4d5130424edcc8508311
damianhunt <dhuntsa@gmail.com>
2022-05-19 23:46:27 +0200

    commit

--- w05_dom_Manipulation/js/script.js
+++ w05_dom_Manipulation/js/script.js
@@ -1,6 +1,3 @@
-
-
-
 const list = document.querySelector("ul");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
@@ -11,13 +8,19 @@ button.addEventListener("click", () => {
 	const listItem = document.createElement("li");
 	const listText = document.createElement("span");
 	const listBtn = document.createElement("button");
+	listBtn.setAttribute("id", "add");
 	listItem.appendChild(listText);
 	listText.textContent = myItem;
 	listItem.appendChild(listBtn);
+
 	listBtn.textContent = "❌";
 	list.appendChild(listItem);
 	listBtn.addEventListener("click", () => {
 		list.removeChild(listItem);
 	});
+	let el = document.getElementById("add");
+	console.log(el.ariaLabel); // "Close"
+	el.ariaLabel = "Chapters: " + myItem;
+	console.log(el.ariaLabel); // "Close dialog"
 	input.focus();
 });
