63c7370dfcb7042e258a015b580056759db58cbb
damianhunt <dhuntsa@gmail.com>
2022-06-19 22:01:54 +0200

    confirm

--- lesson09/js/directory.js
+++ lesson09/js/directory.js
@@ -49,3 +49,17 @@ function displayBusinesses(company) {
     document.querySelector('div.cards').appendChild(card);
 }
 
+/*button*/
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".cards");
+
+listbutton.addEventListener("click", () => {
+    display.classList.add("list");
+    display.classList.remove("cards");
+});
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("cards");
+    display.classList.remove("list");
+})
