53e0b560a032dc9d2ec3c83a100cf46e45d98509
damianhunt <dhuntsa@gmail.com>
2022-06-23 22:13:18 +0200

    confirm

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -3,7 +3,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q={Fairbanks}&appid={da0e357a04e39a1b8b0372f681808fd9}&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=da0e357a04e39a1b8b0372f681808fd9&units=imperial';
 
 async function apiFetch() {
     try {
@@ -21,3 +21,14 @@ async function apiFetch() {
   }
   
   apiFetch();
+
+function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
