e5ae59b99250f7d5d2b45ecfce9fa8d41bd1ff02
damianhunt <dhuntsa@gmail.com>
2022-06-25 15:48:35 +0200

    confirm

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -9,16 +9,16 @@ async function apiFetch() {
     try {
       const response = await fetch(url);
       if (response.ok) {
-        const data = await response.json();
-        console.log(data); // this is for testing the call
-        displayResults(data);
+          const data = await response.json();
+          console.log(data); // this is for testing the call
+          displayResults(data);
       } else {
           throw Error(await response.text());
       }
     } catch (error) {
         console.log(error);
     }
-  }
+}
   
 apiFetch();
 
